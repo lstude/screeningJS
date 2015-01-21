1c20b338e209401853f0597c89556bd6e4e1786a
Laura Stude <laura@clearhead.me>
2015-01-21 13:17:44 -0600

    My first pass - no Googles

--- 576723/screening.js
+++ 576723/screening.js
@@ -5,8 +5,7 @@ if (foo) {
   bar.doSomethingElse(el);
 }
 
-
-
+// I'm not really sure. I think you can use : as a sort of "or" statement
 
 // 2: what is the faulty logic in the following code?
 var foo = 'hello';
@@ -16,7 +15,7 @@ var foo = 'hello';
   console.log(foo);
 })();
 
-
+// the foo inside the function doesn't reference the global foo
 
 
 // 3: given the following code, how would you override the value of the bar
@@ -36,7 +35,14 @@ Thinger.prototype = {
 var foo = new Thinger(),
     bim = new Thinger();
 
-
+// 1) Override the value of the bar property for the variable foo, don't mess up bim
+//    foo.bar = "override";
+// 2) Alter the value of the bar property for foo AND bim
+//    Thinger.prototype.bar = "override";
+// 3) Add a method to foo and bim to console.log the value of their bar properties.
+//    Add a function somewhere?
+// 4) How do you know if Thinger's bar property has been overridden?
+//    Call Thinger.prototype.bar and see what it returns?
 
 
 // 4: given the following code, and assuming that each defined object has a
@@ -48,7 +54,8 @@ var myObjects = {
   widget : new myApp.Widget()
 };
 
-
+// Is there some sort of .destroy method like in Rails? Because if so,
+// that would be very helpful
 
 
 // 5: given the following array, create an array that contains the contents of
@@ -58,7 +65,18 @@ var myObjects = {
 // available)
 var myArray = [ 'foo', 'bar', 'baz' ];
 
-
+// var fooArray = myArray[0] * 3;
+// console.log(fooArray);
+// 
+// var barArray = myArray[1] * 3;
+// console.log(barArray);
+// 
+// var bazArray = myArray[2] * 3;
+// console.log(bazArray);
+// 
+// These would each return NaN because the strings aren't numeric
+// Wondering if a better approach would be to write a loop for it
+// that iterated over each item in the array and printed it 3x
 
 
 // 6: how could you improve the following code?
@@ -74,6 +92,12 @@ $(document).ready(function() {
   $('.foo #bar').click();
 });
 
+// finally, one I can do!
+// $(document).ready(function() {
+//   $('.foo #bar').css({'color':'red','border': '1px solid blue'}).text('new text!').on('click', function () {
+//     $(this).attr('title', 'new tile').width('100px');
+//   });
+// });
 
 
 
@@ -93,7 +117,7 @@ $(document).ready(function() {
   }
 })();
 
-
+// is foo always going to exist/be defined inside .xhrGet?
 
 
 // 8: how could you rewrite the following code to make it shorter?
@@ -104,7 +128,10 @@ $(document).ready(function() {
   $('li.bop a').attr('title', 'i am bop');
 })(dojo, dojo.query);
 
-
+// loop over it. 
+// define each 'thing' (i.e. - foo, bar, baz, bop are things)
+// when li has a length, loop over each one
+// add a + title + 'i am' + thing
 
 
 // 9: how would you improve the following code?
@@ -113,7 +140,7 @@ for (i = 0; i <= 100; i++) {
   $('#gizmo').append('<p><span class="gizmo">i am gizmo ' + i + '</span></p>');
 }
 
-
+// $('#thinger').each()
 
 
 // 10: a user enters their desired tip into a text box; the baseTotal, tax,
@@ -123,7 +150,7 @@ function calculateTotal(baseTotal, tip, tax, fee) {
   return baseTotal + tip + tax + fee;
 }
 
-
+// do we need some sort of validation on the number the person enters as the tip?
 
 
 // 11: given the following data structure, write code that returns an array 
@@ -164,13 +191,19 @@ var menuItems = [
   }
 ];
 
-
+// can you iterate over the menuItems, extract each by it's index 
+// push that into an array and then join all the arrays?
 
 
 // BONUS: write code such that the following alerts "Hello World"
 say('Hello')('World');
 
+// function say() {
+//   helloWorld = "Hello World";
+//   alert(helloWorld);
+// }
 
+// say();
 
 
 // BONUS: what is the faulty logic in the following code? how would you fix it?
